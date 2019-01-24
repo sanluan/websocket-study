f8e550928521e0c26fd28981803e8e07b1ea7ff4
sanluan <master@sanluan.com>
2019-01-24 19:50:44 +0800

    测试用例

---
+++ applications/demo/src/main/java/test/MyInitializer.java
@@ -0,0 +1,26 @@
+package test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.microprofile.message.ThinMessageServer;
+import org.microprofile.message.initializer.ThinAppHandler;
+import org.microprofile.message.initializer.ThinAppInitializer;
+import org.microprofile.websocket.handler.MessageHandler;
+
+public class MyInitializer implements ThinAppInitializer {
+    private ThinMessageServer server;
+
+    public void start(String appPath, ThinAppHandler handler) {
+        server = handler.getMessageServer();
+    }
+
+    public Map<MessageHandler, String[]> register() {
+        HashMap<MessageHandler, String[]> map = new HashMap<>();
+        map.put(new MyMessageHandler(server), new String[] { "/*" });
+        return map;
+    }
+
+    public void stop() {
+    }
+}
---
+++ applications/demo/src/main/java/test/MyMessageHandler.java
@@ -0,0 +1,164 @@
+package test;
+
+import java.io.IOException;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.microprofile.common.constant.Constants;
+import org.microprofile.message.ThinMessageServer;
+import org.microprofile.websocket.handler.MessageHandler;
+import org.microprofile.websocket.handler.Session;
+
+public class MyMessageHandler implements MessageHandler {
+    private ThinMessageServer server;
+    Map<String, User> map = new HashMap<>();
+
+    /**
+     * @param server
+     * 
+     */
+    public MyMessageHandler(ThinMessageServer server) {
+        this.server = server;
+    }
+
+    public void onMessage(byte[] message, Session session) throws IOException {
+        onMessage(new String(message, Constants.DEFAULT_CHARSET), session);
+    }
+
+    public void onMessage(String message, Session session) throws IOException {
+        if (1 < message.length() && ':' == message.charAt(1)) {
+            char command = message.charAt(0);
+            message = message.substring(2);
+            switch (command) {
+            case 'a': {
+                if ("admin".equals(message)) {
+                    User user = map.get(session.getId());
+                    if (null != user) {
+                        user.setMaster(true);
+                        session.sendString("s:login success!");
+                    }
+                }
+                break;
+            }
+            case 'l': {
+                User user = map.get(session.getId());
+                if (null != user && user.isMaster()) {
+                    String[] paths = message.split(",");
+                    if (1 == paths.length) {
+                        server.load(paths[0]);
+                    } else if (2 == paths.length) {
+                        server.load(paths[0], paths[1]);
+                    } else {
+                        session.sendString("s:error path");
+                    }
+                } else {
+                    session.sendString("s:no auth");
+                }
+                break;
+            }
+            case 'd': {
+                User user = map.get(session.getId());
+                if (null != user && user.isMaster()) {
+                    String[] paths = message.split(",");
+                    if (1 == paths.length) {
+                        server.unLoad(paths[0]);
+                    } else {
+                        session.sendString("s:error path");
+                    }
+                } else {
+                    session.sendString("s:no auth");
+                }
+                break;
+            }
+            case 'b': {
+                User user = map.get(session.getId());
+                if (null != user && user.isMaster()) {
+                    user = map.remove(message);
+                    if (null != user) {
+                        user.getSession().close();
+                        session.sendString("s:bye to:" + message);
+                    } else {
+                        session.sendString("s:not online:" + message);
+                    }
+                } else {
+                    session.sendString("s:no auth");
+                }
+                break;
+            }
+            case 'u': {
+                session.sendString("s:" + map.keySet().toString());
+                break;
+            }
+            case 'g': {
+                Collection<User> users = map.values();
+                for (User u : users) {
+                    if (u.getSession() != session) {
+                        u.getSession().sendString("g:" + session.getId() + ":" + message);
+                    }
+                }
+                break;
+            }
+            case 't': {
+                int index = message.indexOf(":");
+                if (0 < index) {
+                    User user = map.get(message.substring(0, index));
+                    if (null != user) {
+                        user.getSession().sendString("u:" + session.getId() + ":" + message.substring(index + 1));
+                    } else {
+                        session.sendString("s:not online:" + message);
+                    }
+                }
+                break;
+            }
+            default:
+                session.sendString("s:error message");
+            }
+        }
+    }
+
+    public void onOpen(Session session) throws IOException {
+        User user = new User();
+        user.setSession(session);
+        map.put(session.getId(), user);
+    }
+
+    public void onClose(Session session) throws IOException {
+        map.remove(session.getId());
+    }
+
+    class User {
+        private Session session;
+        boolean master;
+
+        /**
+         * @return the session
+         */
+        public Session getSession() {
+            return session;
+        }
+
+        /**
+         * @param session
+         *            the session to set
+         */
+        public void setSession(Session session) {
+            this.session = session;
+        }
+
+        /**
+         * @return the master
+         */
+        public boolean isMaster() {
+            return master;
+        }
+
+        /**
+         * @param master
+         *            the master to set
+         */
+        public void setMaster(boolean master) {
+            this.master = master;
+        }
+    }
+}
