3726983c13202565f3e1d729beca858c87edf85f
sanluan <master@sanluan.com>
2019-01-30 22:43:17 +0800

    完善业务逻辑

--- applications/demo/src/main/java/com/publiccms/message/MyMessageHandler.java
+++ applications/demo/src/main/java/com/publiccms/message/MyMessageHandler.java
@@ -1,18 +1,36 @@
 package com.publiccms.message;
 
 import java.io.IOException;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.microprofile.common.constant.Constants;
 import org.microprofile.message.ThinMessageServer;
 import org.microprofile.websocket.handler.MessageHandler;
 import org.microprofile.websocket.handler.Session;
 
 public class MyMessageHandler implements MessageHandler {
+    protected final Log log = LogFactory.getLog(getClass());
+    @SuppressWarnings("unused")
     private ThinMessageServer server;
-    Map<String, User> map = new HashMap<>();
+    Map<Integer, User> userMap = new HashMap<>();
+    private ConcurrentLinkedQueue<Integer> userIdQueue = new ConcurrentLinkedQueue<>();
+    private Map<String, User> sessionMap = new HashMap<>();
+    private Session adminSession;
+    private String password;
+    private int i;
+
+    public int getNextUserId() {
+        Integer userId = userIdQueue.poll();
+        if (null == userId) {
+            return ++i;
+        }
+        return userId;
+    }
 
     /**
      * @param server
@@ -20,6 +38,8 @@ public class MyMessageHandler implements MessageHandler {
      */
     public MyMessageHandler(ThinMessageServer server) {
         this.server = server;
+        password = UUID.randomUUID().toString();
+        log.warn("password:" + password);
     }
 
     public void onMessage(byte[] message, Session session) throws IOException {
@@ -32,54 +52,20 @@ public class MyMessageHandler implements MessageHandler {
             message = message.substring(2);
             switch (command) {
             case 'a': {
-                if ("admin".equals(message)) {
-                    User user = map.get(session.getId());
-                    if (null != user) {
-                        user.setMaster(true);
-                        session.sendString("s:login success!");
-                    }
-                }
-                break;
-            }
-            case 'l': {
-                User user = map.get(session.getId());
-                if (null != user && user.isMaster()) {
-                    String[] paths = message.split(",");
-                    if (1 == paths.length) {
-                        server.load(paths[0]);
-                    } else if (2 == paths.length) {
-                        server.load(paths[0], paths[1]);
-                    } else {
-                        session.sendString("s:error path");
-                    }
-                } else {
-                    session.sendString("s:no auth");
+                if (password.equals(message)) {
+                    adminSession = session;
+                    session.sendString("s:login success!");
                 }
                 break;
             }
             case 'p': {
-                session.sendString(message);
-                break;
-            }
-            case 'd': {
-                User user = map.get(session.getId());
-                if (null != user && user.isMaster()) {
-                    String[] paths = message.split(",");
-                    if (1 == paths.length) {
-                        server.unLoad(paths[0]);
-                    } else {
-                        session.sendString("s:error path");
-                    }
-                } else {
-                    session.sendString("s:no auth");
-                }
                 break;
             }
             case 'b': {
-                User user = map.get(session.getId());
-                if (null != user && user.isMaster()) {
-                    user = map.remove(message);
+                if (null != adminSession && adminSession == session) {
+                    User user = sessionMap.remove(session.getId());
                     if (null != user) {
+                        userMap.remove(user.getId());
                         user.getSession().close();
                         session.sendString("s:bye to:" + message);
                     } else {
@@ -91,11 +77,15 @@ public class MyMessageHandler implements MessageHandler {
                 break;
             }
             case 'u': {
-                session.sendString("s:" + map.keySet().toString());
+                if (null != adminSession && adminSession == session) {
+                    session.sendString("s:" + userMap.keySet().toString());
+                } else {
+                    session.sendString("s:no auth");
+                }
                 break;
             }
             case 'n': {
-                User user = map.get(session.getId());
+                User user = sessionMap.get(session.getId());
                 if (null != user && null != message && 0 < message.length() && 100 > message.length()) {
                     user.setNickName(message);
                     session.sendString("s:operate success!");
@@ -104,47 +94,53 @@ public class MyMessageHandler implements MessageHandler {
                 }
                 break;
             }
-            case 'g': {
-                Collection<User> users = map.values();
-                for (User u : users) {
-                    if (u.getSession() != session) {
-                        u.getSession().sendString("g:" + session.getId() + ":" + message);
-                    }
-                }
-                break;
-            }
-            case 't': {
-                int index = message.indexOf(":");
-                if (0 < index) {
-                    User user = map.get(message.substring(0, index));
-                    if (null != user) {
-                        user.getSession().sendString("u:" + session.getId() + ":" + message.substring(index + 1));
-                    } else {
-                        session.sendString("s:not online:" + message);
-                    }
-                }
-                break;
-            }
             default:
                 session.sendString("s:error message");
             }
+        } else if (null != adminSession) {
+            User user = sessionMap.get(session.getId());
+            adminSession.sendString(user.getNickName() + ",编号" + user.getId() + " 说:" + message);
+        } else {
+            session.sendString("客服不在线哦");
         }
     }
 
     public void onOpen(Session session) throws IOException {
         User user = new User();
         user.setSession(session);
-        map.put(session.getId(), user);
+        user.setId(getNextUserId());
+        sessionMap.put(session.getId(), user);
+        userMap.put(user.getId(), user);
     }
 
     public void onClose(Session session) throws IOException {
-        map.remove(session.getId());
+        User user = sessionMap.remove(session.getId());
+        userMap.remove(user.getId());
+        userIdQueue.add(user.getId());
+        if (null != adminSession && adminSession == session) {
+            adminSession = null;
+        }
     }
 
     class User {
         private Session session;
         private String nickName;
-        boolean master;
+        private int id;
+
+        /**
+         * @return the i
+         */
+        public int getId() {
+            return id;
+        }
+
+        /**
+         * @param id
+         *            the id to set
+         */
+        public void setId(int id) {
+            this.id = id;
+        }
 
         /**
          * @return the session
@@ -159,21 +155,6 @@ public class MyMessageHandler implements MessageHandler {
          */
         public void setSession(Session session) {
             this.session = session;
-        }
-
-        /**
-         * @return the master
-         */
-        public boolean isMaster() {
-            return master;
-        }
-
-        /**
-         * @param master
-         *            the master to set
-         */
-        public void setMaster(boolean master) {
-            this.master = master;
         }
 
         /**
